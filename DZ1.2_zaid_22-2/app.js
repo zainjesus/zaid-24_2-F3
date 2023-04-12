4da7740ba3a0d8e21d7ee02949733b73d1b68f9b
zainjesus <113356414+zainjesus@users.noreply.github.com>
2023-04-12 14:50:57 +0600

    DZ1

---
+++ DZ1.2_zaid_22-2/app.js
@@ -0,0 +1,15 @@
+const block = document.querySelector(".block");
+
+window.addEventListener("load", function() {
+    setTimeout(moveBlock, 1000);
+  });
+  
+
+function moveBlock() {
+    const currentPos = block.offsetLeft;
+    const windowWidth = window.innerWidth;
+    if (currentPos < windowWidth - block.offsetWidth) {
+        block.style.left = currentPos + 10 + "px";
+        setTimeout(moveBlock, 10);
+    }
+}
